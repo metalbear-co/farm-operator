9bfb829424e9ea6f81eac08eb164e845e3e7c764
Dmitry Dodzin <d.dodzin@gmail.com>
2023-02-26 18:29:18 +0200

    Step 2

---
+++ step-2/src/main.rs
@@ -0,0 +1,59 @@
+#![feature(once_cell)]
+
+use std::net::SocketAddr;
+
+use axum::{response::IntoResponse, routing::get, Json, Router};
+use axum_server::tls_rustls::RustlsConfig;
+use k8s_openapi::apimachinery::pkg::apis::meta::v1::{APIResource, APIResourceList};
+use kube::Resource as _;
+use tower_http::trace::TraceLayer;
+
+use crate::resources::llama;
+
+mod resources;
+
+async fn get_api_resources() -> impl IntoResponse {
+    Json(APIResourceList {
+        group_version: "farm.example.com/v1alpha".to_owned(),
+        resources: vec![APIResource {
+            group: Some(llama::Llama::group(&()).into()),
+            kind: llama::Llama::kind(&()).into(),
+            name: llama::Llama::plural(&()).into(),
+            namespaced: true,
+            verbs: vec!["list".to_owned(), "pet".to_owned()],
+            ..Default::default()
+        }],
+    })
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/apis/farm.example.com/v1alpha", get(get_api_resources))
+        .route(
+            "/apis/farm.example.com/v1alpha/namespaces/:namespace/llamas",
+            get(llama::list_llamas),
+        )
+        .route(
+            "/apis/farm.example.com/v1alpha/namespaces/:namespace/llamas/:name",
+            get(llama::get_llama),
+        )
+        .layer(TraceLayer::new_for_http());
+
+    let cert = rcgen::generate_simple_self_signed(vec!["localhost".to_owned()])?;
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let config = RustlsConfig::from_der(
+        vec![cert.serialize_der()?],
+        cert.serialize_private_key_der(),
+    )
+    .await?;
+    println!("listening on {addr}");
+
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
+        .await
+        .map_err(anyhow::Error::from)
+}
---
+++ step-2/src/resources/llama.rs
@@ -0,0 +1,66 @@
+use std::{collections::HashMap, sync::LazyLock};
+
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use chrono::Utc;
+use k8s_openapi::apimachinery::pkg::apis::meta::v1::ListMeta;
+use kube::CustomResource;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+type Llamas = HashMap<String, HashMap<String, Llama>>;
+
+static STATIC_LLAMAS: LazyLock<Llamas> = LazyLock::new(|| {
+    serde_json::from_value(serde_json::json!({
+        "default": {
+            "dolly": {
+                "metadata": {
+                    "name": "dolly",
+                    "namespace": "default",
+                    "creationTimestamp": Utc::now(),
+                },
+                "spec": {
+                    "height": 0.5,
+                    "weight": 31.4
+                }
+            }
+        }
+    }))
+    .expect("Could not create static lamas")
+});
+
+#[derive(CustomResource, Clone, Debug, Deserialize, Serialize, JsonSchema)]
+#[kube(
+    group = "farm.example.com",
+    version = "v1alpha",
+    kind = "Llama",
+    namespaced
+)]
+pub struct LlamaSpec {
+    pub weight: f32,
+    pub height: f32,
+}
+
+pub async fn list_llamas(Path(namespace): Path<String>) -> impl IntoResponse {
+    Json(serde_json::json!({
+        "apiVersion": "farm.example.com/v1alpha",
+        "kind": "LamaList",
+        "items": &STATIC_LLAMAS.get(&namespace).map(|lamas| lamas.values().collect::<Vec<_>>()).unwrap_or_default(),
+        "metadata": ListMeta::default()
+    }))
+}
+
+pub async fn get_llama(Path((namespace, name)): Path<(String, String)>) -> Response {
+    if let Some(lama) = STATIC_LLAMAS
+        .get(&namespace)
+        .and_then(|lamas| lamas.get(&name))
+    {
+        Json(lama).into_response()
+    } else {
+        StatusCode::NOT_FOUND.into_response()
+    }
+}
