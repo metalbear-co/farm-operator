f144485f9288dc8fc1f5819956cefab4c67aeecc
Dmitry Dodzin <d.dodzin@gmail.com>
2023-03-01 14:33:32 +0200

    to_owned -> to_string

--- step-1/src/main.rs
+++ step-1/src/main.rs
@@ -6,7 +6,7 @@ use k8s_openapi::apimachinery::pkg::apis::meta::v1::APIResourceList;
 
 async fn get_api_resources() -> impl IntoResponse {
     Json(APIResourceList {
-        group_version: "farm.example.com/v1alpha".to_owned(),
+        group_version: "farm.example.com/v1alpha".to_string(),
         resources: vec![],
     })
 }
@@ -17,7 +17,7 @@ async fn main() -> anyhow::Result<()> {
 
     // We generate a self-signed certificate for example purposes in a proper service this should be
     // loaded from secret and CA for said cert should be defined in APIService uner `caBundle`
-    let tls_cert = rcgen::generate_simple_self_signed(vec!["localhost".to_owned()])?;
+    let tls_cert = rcgen::generate_simple_self_signed(vec!["localhost".to_string()])?;
     let tls_config = RustlsConfig::from_der(
         vec![tls_cert.serialize_der()?],
         tls_cert.serialize_private_key_der(),
--- step-2/src/main.rs
+++ step-2/src/main.rs
@@ -13,13 +13,13 @@ mod resources;
 
 async fn get_api_resources() -> impl IntoResponse {
     Json(APIResourceList {
-        group_version: "farm.example.com/v1alpha".to_owned(),
+        group_version: "farm.example.com/v1alpha".to_string(),
         resources: vec![APIResource {
             group: Some(llama::Llama::group(&()).into()),
             kind: llama::Llama::kind(&()).into(),
             name: llama::Llama::plural(&()).into(),
             namespaced: true,
-            verbs: vec!["list".to_owned(), "get".to_owned()],
+            verbs: vec!["list".to_string(), "get".to_string()],
             ..Default::default()
         }],
     })
@@ -40,7 +40,7 @@ async fn main() -> anyhow::Result<()> {
 
     // We generate a self-signed certificate for example purposes in a proper service this should be
     // loaded from secret and CA for said cert should be defined in APIService uner `caBundle`
-    let tls_cert = rcgen::generate_simple_self_signed(vec!["localhost".to_owned()])?;
+    let tls_cert = rcgen::generate_simple_self_signed(vec!["localhost".to_string()])?;
     let tls_config = RustlsConfig::from_der(
         vec![tls_cert.serialize_der()?],
         tls_cert.serialize_private_key_der(),
